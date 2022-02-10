5a2cf614ebf73d315c2ba15944275d0d9681ab0d
walpolerobotics <roborebels1153@gmail.com>
2022-02-09 20:07:18 -0500

    Digital Input Sensors

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -6,27 +6,47 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class Collector extends SubsystemBase {
 
   private WPI_TalonFX collect;
   private DoubleSolenoid doubleSolenoid1;
+  private WPI_TalonFX indexerA;
+  private WPI_TalonFX indexerB;
+  private WPI_TalonFX indexerC;
+  private WPI_TalonFX indexerD;
+  private DigitalInput collectorSensor;
+  private DigitalInput indexerSensor;
+  private DigitalInput shooterSensor;
 
-
-
-
-  /** Creates a new Climber. */
+  /** Creates a new Collector. */
   public Collector() {
     collect = new WPI_TalonFX(4);
     doubleSolenoid1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1 );
+    collectorSensor = new DigitalInput(0);
+    indexerSensor = new DigitalInput(1);
+    shooterSensor = new DigitalInput(2);
   }
-  
 
+  public void indexerOn() {
+    indexerA.set(ControlMode.PercentOutput, 0.5);
+    indexerB.set(ControlMode.PercentOutput, 0.5);
+    indexerC.set(ControlMode.PercentOutput, 0.5);
+    indexerD.set(ControlMode.PercentOutput, 0.5);
+  }
+
+  public void indexerOff() {
+    indexerA.set(ControlMode.PercentOutput, 0);
+    indexerB.set(ControlMode.PercentOutput, 0);
+    indexerC.set(ControlMode.PercentOutput, 0);
+    indexerD.set(ControlMode.PercentOutput, 0);
+  }
   
   //This is a helper method that clarifies what winching up means in the context of the set method; 
   //might need to be inverted depending on motor orientation
@@ -61,6 +81,17 @@ public class Collector extends SubsystemBase {
     doubleSolenoid1.toggle();
   }
 
+  public boolean getCollectorSensor(){
+    return collectorSensor.get();
+  }
+
+  public boolean getShooterSensor(){
+    return shooterSensor.get();
+  }
+
+  public boolean getIndexerSensor(){
+    return indexerSensor.get();
+  }
 
   @Override
   public void periodic() {
